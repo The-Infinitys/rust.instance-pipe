5c632f61ad9f5dd02c9dd8a85cb9b287248b2b5c
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-08-01 10:41:54 +0900

    Auto-save: Changed 7 lines in data.md show.sh  on 2025-08-01 10:41:54

--- show.sh
+++ show.sh
@@ -5,6 +5,7 @@ if [ $# -ne 2 ]; then
     echo "使い方: $0 ディレクトリ 拡張子"
     exit 1
 fi
+parent=`pwd` 
 
 directory=$1
 extension=$2
@@ -14,13 +15,15 @@ if [ ! -d "$directory" ]; then
     echo "エラー: ディレクトリ $directory が存在しません"
     exit 1
 fi
-
+echo \# $parent
+echo ""
 # ファイルを検索して表示
 find "$directory" -type f -name "*.$extension" | while read -r file; do
-    echo "$file"
+    echo "## $file"
     echo ""
     echo "\`\`\`"
     cat $file
+    echo ""
     echo "\`\`\`"
     echo ""
 done
